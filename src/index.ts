d03224e1b44e8041b1360e74c02507fe3161c0dc
Dylan Cawsey <dylancawsey@gmail.com>
2022-03-25 15:42:43 +1100

    ts-node not liking top-level await so nodemon just re-compiles the typescript and runs vanilla node

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('Hello world!');
+#!/usr/bin/env node
+import inquirer from 'inquirer';
+
+const askProjectName = async () => {
+  const { projectName } = await inquirer.prompt({
+    name: 'projectName',
+    type: 'input',
+    message: 'What would you like to name your project directory?',
+    default() {
+      return 'ts-node-project';
+    },
+  });
+
+  console.log(`Thanks faor providing: ${projectName}`);
+};
+
+await askProjectName();
