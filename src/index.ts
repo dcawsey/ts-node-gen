e4e124c7ad8f4c27f242810bd0cbf4af49dd16a8
Dylan Cawsey <dylancawsey@gmail.com>
2022-03-25 17:06:26 +1100

    Creates required directories and copies config files from current repo for cli

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
 import inquirer from 'inquirer';
+import fs from 'fs';
 
-const askProjectName = async () => {
+const askProjectName = async (): Promise<string> => {
   const { projectName } = await inquirer.prompt({
     name: 'projectName',
     type: 'input',
@@ -11,7 +12,51 @@ const askProjectName = async () => {
     },
   });
 
-  console.log(`Thanks faor providing: ${projectName}`);
+  return projectName;
 };
 
-await askProjectName();
+const createDirectory = async (projectName: string): Promise<void> => {
+  const directoryPath = `./${projectName}`;
+
+  if (fs.existsSync(directoryPath)) {
+    throw new Error('Directory with the supplied project name already exists');
+  }
+
+  fs.mkdirSync(directoryPath);
+};
+
+const copyConfigFiles = async (projectName: string): Promise<void> => {
+  const filesInDirectory = fs.readdirSync('./');
+
+  console.log('\nCopying config files to project directory...');
+
+  filesInDirectory.forEach((file: string) => {
+    if (!fs.lstatSync(`./${file}`).isDirectory()) {
+      console.log(file);
+      fs.copyFileSync(`./${file}`, `./${projectName}/${file}`);
+    }
+  });
+};
+
+const createProject = async (projectName: string): Promise<void> => {
+  await createDirectory(projectName);
+  await copyConfigFiles(projectName);
+  await createDirectory(`${projectName}/src`);
+};
+
+const run = async () => {
+  try {
+    const projectName = await askProjectName();
+
+    await createProject(projectName);
+  } catch (error) {
+    if (error instanceof Error) {
+      console.log('\nFailed to create project:', error.message);
+      return;
+    }
+
+    throw error;
+  }
+};
+
+await run();
